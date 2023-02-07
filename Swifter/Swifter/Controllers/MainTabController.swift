d776224a9b76e59e1d6e898b33d13b98cb6a5d12
Dario Santos <dario.santos@hotmail.com>
2023-02-07 10:46:16 +0000

    feat: initial config for mvvm architecture and no storyboards

---
+++ Swifter/Swifter/Controllers/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  Swifter
+//
+//  Created by Santos, Dario Ferreira on 07/02/2023.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
